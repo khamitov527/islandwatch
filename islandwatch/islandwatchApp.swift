aa5b7e89a680e5f6ecc4d8574713f6c81b237580
khamitov527 <khamitov527@gmail.com>
2025-04-09 18:30:54 -0400

    Initial Commit

---
+++ islandwatch/islandwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  islandwatchApp.swift
+//  islandwatch
+//
+//  Created by Akylbek Khamitov on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct islandwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ islandwatchTests/islandwatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  islandwatchTests.swift
+//  islandwatchTests
+//
+//  Created by Akylbek Khamitov on 4/9/25.
+//
+
+import Testing
+@testable import islandwatch
+
+struct islandwatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
